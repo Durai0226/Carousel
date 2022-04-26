7288817bb61ef0e6d07215da9452c5cc1a6eb163
JoesCorner <95665714+JoesCorner@users.noreply.github.com>
2022-04-26 11:17:43 +0530

    Add files via upload

---
+++ caro/js/js.js
@@ -0,0 +1,7 @@
+let button = document.querySelector(".arrow"),
+    links = document.querySelector(".nav-links1")
+   
+
+    button.addEventListener("click",()=> {
+          links.classList.toggle("display")
+    })
